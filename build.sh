f90c6ae0320ac26e42b6e0f903fa6c1fe84cc257
sébastien Gonin <sebastiengonin@MacBookAir.lan>
2025-07-02 22:29:06 +0200

    Ajout fichiers de configuration pour Render

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# Script de build pour Render
+
+# Installer les dépendances avec poetry
+poetry install
+
+# Dossier à exécuter ensuite si tu as un app.py
+# poetry run python app.py
