37cddd7f24ba573873a0343bc921b488fb2372f6
sébastien Gonin <sebastiengonin@MacBookAir.lan>
2025-07-02 23:00:46 +0200

    Adaptation du port Flask pour Render

--- app.py
+++ app.py
@@ -82,5 +82,7 @@ def news():
 def guide():
     return render_template('guide.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
