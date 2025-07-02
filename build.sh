29ea5eced2df376d823321a35260829c133d7c50
sébastien Gonin <sebastiengonin@MacBookAir.lan>
2025-07-02 22:59:07 +0200

    Préparation propre pour Render (Poetry + Flask)

--- build.sh
+++ build.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
-# Script de build pour Render
 
-# Installer les dépendances avec poetry
-poetry install
-
-# Dossier à exécuter ensuite si tu as un app.py
-# poetry run python app.py
+poetry install --no-interaction --no-ansi
