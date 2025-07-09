1f527a8a333abfd75af38176695d63a31fa3c2b6
sébastien Gonin <sebastiengonin@MacBookAir.lan>
2025-07-09 14:49:12 +0200

    Mise à jour des headers de sécurité (CSP-friendly)

--- app.py
+++ app.py
@@ -21,8 +21,8 @@ def save_likes(likes):
 def add_security_headers(response):
     response.headers['Strict-Transport-Security'] = "max-age=31536000; includeSubDomains"
     response.headers['X-Content-Type-Options'] = "nosniff"
-    response.headers['X-Frame-Options'] = "DENY"
-    response.headers['Referrer-Policy'] = "no-referrer"
+    response.headers['X-Frame-Options'] = "SAMEORIGIN"
+    response.headers['Referrer-Policy'] = "strict-origin-when-cross-origin"
     response.headers['Permissions-Policy'] = "geolocation=(), microphone=(), camera=()"
     return response
 
